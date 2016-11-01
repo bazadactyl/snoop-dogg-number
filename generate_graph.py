1003746520e1637dff3770730d8488d877d22d7a
Basim Ramadhan <basim.ramadhan@gmail.com>
2016-10-31 21:04:29 -0400

    Graph generation script now outputs JSON

--- generate_graph.py
+++ generate_graph.py
@@ -3,7 +3,32 @@
 import psycopg2
 import networkx as nx
 import pickle
-import threading
+import json
+from networkx.readwrite import json_graph
+
+def write_json_nodelink(graph):
+    node_link_data = json_graph.node_link_data(graph)
+    with open('graph/graph.nodelink.json', 'w') as outfile:
+        json.dump(node_link_data, outfile, indent=2)
+
+def write_json_adjacency(graph):
+    adjacency_data = json_graph.adjacency_data(graph)
+    with open('graph/graph.adjacency.json', 'w') as outfile:
+        json.dump(adjacency_data, outfile, indent=2)
+
+def write_other_formats(graph):
+    nx.write_adjlist(graph, "graph/graph.adjlist", delimiter='||')
+    nx.write_multiline_adjlist(graph, "graph/graph.multi.adjlist", delimiter='||')
+    nx.write_edgelist(graph, "graph/graph.edgelist", delimiter='||')
+    nx.write_gexf(graph, "graph/graph.gexf")
+    nx.write_gml(graph, "graph/graph.gml")
+    nx.write_gml(graph, "graph/graph.gml.gz")
+    nx.write_gpickle(graph, "graph/graph.gpickle")
+    nx.write_graphml(graph, "graph/graph.graphml")
+    nx.write_yaml(graph, "graph/graph.yaml")
+    # nx.write_graph6(graph, "graph/graph.graph6") # https://github.com/networkx/networkx/issues/2295
+    nx.write_sparse6(graph, "graph/graph.sparse6")
+    nx.write_pajek(graph, "graph/graph.pajek")
 
 # Connect to the MusicBrainz database
 connection = psycopg2.connect(database="musicbrainz", user="musicbrainz", password="", host="musicbrainz", port="5432")
@@ -21,28 +46,17 @@ cursor.execute('''
 
 # Incrementally populate the graph with edges
 while True:
-    cursor.execute("FETCH 1000 FROM db_cursor")
+    cursor.execute("FETCH 10000 FROM db_cursor")
     edges = cursor.fetchall()
-
     if not edges:
         break
     else:
         graph.add_edges_from(edges)
 
 # Write the graph to disk in several formats
-# TODO: Run each of these operations in a separate thread.
-nx.write_adjlist(graph, "graph/graph.adjlist", delimiter='||')
-nx.write_multiline_adjlist(graph, "graph/graph.multi.adjlist", delimiter='||')
-nx.write_edgelist(graph, "graph/graph.edgelist", delimiter='||')
-nx.write_gexf(graph, "graph/graph.gexf")
-nx.write_gml(graph, "graph/graph.gml")
-nx.write_gml(graph, "graph/graph.gml.gz")
-nx.write_gpickle(graph, "graph/graph.gpickle")
-nx.write_graphml(graph, "graph/graph.graphml")
-nx.write_yaml(graph, "graph/graph.yaml")
-nx.write_graph6(graph, "graph/graph.graph6")
-nx.write_sparse6(graph, "graph/graph.sparse6")
-nx.write_pajek(graph, "graph/graph.pajek")
+write_json_nodelink(graph)
+write_json_adjacency(graph)
+write_other_formats(graph)
 
 # Close the connection
 connection.close()
