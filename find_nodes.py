861e143b08024ed03fd0b19d8d24b95221a13003
Basim Ramadhan <basim.ramadhan@gmail.com>
2016-11-01 11:17:31 -0400

    Created script for populating nodes from edges

---
+++ find_nodes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import psycopg2
+
+connection = psycopg2.connect(database="musicbrainz", user="musicbrainz", password="", host="musicbrainz", port="5432")
+cursor = connection.cursor()
+print("Database opened successfully")
+
+# Delete the old edges table
+cursor.execute("""
+    DROP TABLE IF EXISTS nodes;
+""")
+
+# Create new edges table
+cursor.execute("""
+    CREATE TABLE nodes (
+        artist TEXT NOT NULL,
+        PRIMARY KEY(artist)
+    );
+""")
+
+# Find edges via artist credit and populate the edges table
+query = open("find_nodes.sql").read()
+cursor.execute(query)
+
+# Commit the changes
+connection.commit()
+connection.close()
+print("Done!")
---
+++ find_nodes.sql
@@ -0,0 +1,15 @@
+-- This SQL query populates the nodes table with all the artists who exist in
+-- the edges table. This means that artists who haven't collaborated with anyone
+-- won't appear here.
+
+WITH
+nodes_tmp AS ( -- Each row of this table has a "collaboration" and one of its "collaborators"
+    SELECT collaborator1 AS artist FROM edges
+    UNION ALL
+    SELECT collaborator2 AS artist FROM edges
+)
+
+INSERT INTO nodes (artist)
+    SELECT DISTINCT artist
+    FROM nodes_tmp
+    ORDER BY artist;
